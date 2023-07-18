c682dc716e3725049e08409533b6ef6772c83d37
Mo3Mohd <elmoatasem2020@gmail.com>
2023-07-18 21:05:29 +0200

    Allowed Hosts

--- duststore/duststore/settings.py
+++ duststore/duststore/settings.py
@@ -75,10 +75,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = [
-'http://localhost:3000'
-]
-
 ROOT_URLCONF = 'duststore.urls'
 
 TEMPLATES = [
