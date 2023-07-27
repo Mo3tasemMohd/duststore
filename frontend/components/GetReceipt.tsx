c08fe92e8814cda5dd710888056f31b21dfb9971
Hazem Osama <hazem3500@gmail.com>
2023-07-27 21:10:13 +0300

    fix: Invalid refer code

--- frontend/components/GetReceipt.tsx
+++ frontend/components/GetReceipt.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { Button } from "@/components/ui/button";
-import { useToast } from "@/components/ui/use-toast";
-import { Input } from "@/components/ui/input";
-import { useAuth } from "@/lib/useAuth";
-import { useState } from "react";
+import * as React from 'react';
+import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/use-toast';
+import { Input } from '@/components/ui/input';
+import { useAuth } from '@/lib/useAuth';
+import { useState } from 'react';
 
 // export function GetReceipt() {
 //     const { toast } = useToast();
@@ -66,143 +66,174 @@ import { useState } from "react";
 // }
 
 export function GetReceipt() {
-  const { toast } = useToast();
-  const [receipts, setReceipts] = useState({
-    before15Days: [],
-    after15Days: [],
-  });
-  const [customer, setCustomer] = useState(null);
+    const { toast } = useToast();
+    const [receipts, setReceipts] = useState({
+        before15Days: [],
+        after15Days: [],
+    });
+    const [customer, setCustomer] = useState(null);
 
-  const getReceipts = async (e) => {
-    e.preventDefault();
+    const getReceipts = async (e) => {
+        e.preventDefault();
 
-    try {
-      const first15DayResponse = fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/dust/receits/${e.target.code.value}/first15days`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
+        try {
+            const first15DayResponse = fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/dust/receits/${e.target.code.value}/first15days`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            const last15DayResponse = fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/dust/receits/${e.target.code.value}/last15days`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            const [first15DayPromise, last15DayPromise] = await Promise.all([
+                first15DayResponse,
+                last15DayResponse,
+            ]);
+
+            const {
+                receipts: receipetsBefore15Days,
+                refer_customer: customer,
+            } = await first15DayPromise.json();
+            const receipetsAfter15Days = await last15DayPromise.json();
+
+            if (
+                (receipetsBefore15Days &&
+                    typeof receipetsBefore15Days[0] === 'string') ||
+                (receipetsAfter15Days &&
+                    typeof receipetsAfter15Days[0] === 'string')
+            ) {
+                toast({
+                    title: 'Something went wrong!',
+                    description: receipetsBefore15Days
+                        ? receipetsBefore15Days[0]
+                        : receipetsAfter15Days[0],
+                    variant: 'destructive',
+                });
+                return;
+            }
+
+            if (receipetsBefore15Days || receipetsAfter15Days) {
+                // Set the retrieved receipts in state
+                setReceipts({
+                    before15Days: receipetsBefore15Days,
+                    after15Days: receipetsAfter15Days,
+                });
+
+                setCustomer(customer);
+            }
+        } catch (error) {
+            toast({
+                title: 'Something went wrong!',
+                description: 'Please try again later.',
+                variant: 'destructive',
+            });
         }
-      );
+    };
 
-      const last15DayResponse = fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/dust/receits/${e.target.code.value}/last15days`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+    return (
+        <div className="grid w-full max-w-md items-center gap-4 m-auto mt-20 main-card">
+            <h1 className="font-extrabold text-3xl text-center mb-16">
+                RECEIPT
+            </h1>
 
-      const [first15DayPromise, last15DayPromise] = await Promise.all([
-        first15DayResponse,
-        last15DayResponse,
-      ]);
+            <form
+                onSubmit={getReceipts}
+                className="grid w-full max-w-md items-center gap-4 m-auto"
+            >
+                <Input name="code" placeholder="Code" required />
+                <button type="submit" className="mt-2">
+                    GET RECEIPTS
+                </button>
+            </form>
 
-      const { receipts: receipetsBefore15Days, refer_customer: customer } =
-        await first15DayPromise.json();
-      const receipetsAfter15Days = await last15DayPromise.json();
+            {customer ? <CustomerCard customer={customer} /> : null}
 
-      if (receipetsBefore15Days || receipetsAfter15Days) {
-        // Set the retrieved receipts in state
-        setReceipts({
-          before15Days: receipetsBefore15Days,
-          after15Days: receipetsAfter15Days,
-        });
+            {receipts?.before15Days.length > 0 && (
+                <div className="grid gap-8 mt-8">
+                    <h2 className="text-xl font-bold firstlast">
+                        FIRST 15 DAYS DEALS
+                    </h2>
+                    {receipts?.before15Days.map((receipt) => (
+                        <Receipt key={receipt.id} receipt={receipt} />
+                    ))}
+                </div>
+            )}
 
-        setCustomer(customer);
-      } else {
-
-        toast({
-          title: "Something went wrong!",
-          variant: "destructive",
-        });
-      }
-    } catch (error) {
-      console.log(error); // Log the error message to the console
-      toast({
-        title: "Something went wrong!",
-        description: "Please try again later.",
-        variant: "destructive",
-      });
-    }
-  };
-
-  console.log(customer);
-
-  return (
-    <div className="grid w-full max-w-md items-center gap-4 m-auto mt-20 main-card">
-      <h1 className="font-extrabold text-3xl text-center mb-16">RECEIPT</h1>
-
-      <form
-        onSubmit={getReceipts}
-        className="grid w-full max-w-md items-center gap-4 m-auto"
-      >
-        <Input name="code" placeholder="Code" required />
-        <button type="submit" className="mt-2">
-          GET RECEIPTS
-        </button>
-      </form>
-
-        {customer ? <CustomerCard  customer={customer} /> : null}
-
-      {receipts?.before15Days.length > 0 && (
-        <div className="grid gap-8 mt-8">
-          <h2 className="text-xl font-bold firstlast">FIRST 15 DAYS DEALS</h2>
-          {receipts?.before15Days.map((receipt) => (
-            <Receipt key={receipt.id} receipt={receipt} />
-          ))}
+            {receipts?.after15Days.length > 0 && (
+                <div className="grid gap-8 mt-8">
+                    <h2 className="text-xl">LAST 15 DAYS DEALS</h2>
+                    {receipts?.after15Days.map((receipt) => (
+                        <Receipt key={receipt.id} receipt={receipt} />
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-
-      {receipts?.after15Days.length > 0 && (
-        <div className="grid gap-8 mt-8">
-          <h2 className="text-xl">LAST 15 DAYS DEALS</h2>
-          {receipts?.after15Days.map((receipt) => (
-            <Receipt key={receipt.id} receipt={receipt} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+    );
 }
 
 function Receipt({ receipt }) {
-  return (
-    <div className=" rounded-lg shadow-md p-4 customer-card">
-      <p><strong>NAME:</strong> {receipt.receipt_owner_name}</p>
-      <p>MOBILE: {receipt.receipt_owner_phone}</p>
-      <p>RECEIPT: {receipt.receipt_price}</p>
-      <p>DESCRIPTION: {receipt.receit_description}</p>
-      <p>DEAL DATE: {new Date(receipt.created_at).toLocaleDateString()}</p>
-    </div>
-  );
+    return (
+        <div className=" rounded-lg shadow-md p-4 customer-card">
+            <p>
+                <strong>NAME:</strong> {receipt.receipt_owner_name}
+            </p>
+            <p>MOBILE: {receipt.receipt_owner_phone}</p>
+            <p>RECEIPT: {receipt.receipt_price}</p>
+            <p>DESCRIPTION: {receipt.receit_description}</p>
+            <p>
+                DEAL DATE: {new Date(receipt.created_at).toLocaleDateString()}
+            </p>
+        </div>
+    );
 }
 
 function CustomerCard({ customer }) {
-  const {
-    ReferCustomer_deals_totalpricefirst15days,
-    ReferCustomer_deals_totalpricelast15days,
-    created_at,
-    referCustomer_code,
-    referCustomer_name,
-    referCustomer_phone,
-    referCustomer_receipt,
-  } = customer 
+    const {
+        ReferCustomer_deals_totalpricefirst15days,
+        ReferCustomer_deals_totalpricelast15days,
+        created_at,
+        referCustomer_code,
+        referCustomer_name,
+        referCustomer_phone,
+        referCustomer_receipt,
+    } = customer;
 
-  return (
-    <div className="rounded-lg shadow-md p-4 customer-card">
-      <p>NAME: {referCustomer_name}</p>
-      <p>MOBILE: {referCustomer_phone}</p>
-      <p>REFERAL CODE: {referCustomer_code}</p>
-      <p>RECEIPT: {referCustomer_receipt}</p>
-      <p>DEAL DATE: {new Date(created_at).toLocaleDateString()}</p>
-      <p>FIRST 15 DAYS DEALS: {ReferCustomer_deals_totalpricefirst15days}</p>
-      <p>LAST 15 DAYS DEALS: {ReferCustomer_deals_totalpricelast15days}</p>
-      <p>CASHBACK: {(Math.round((ReferCustomer_deals_totalpricefirst15days*0.1 + ReferCustomer_deals_totalpricelast15days*0.05) * 100) / 100).toFixed(2)} L.E</p>
-    </div>
-  );
+    return (
+        <div className="rounded-lg shadow-md p-4 customer-card">
+            <p>NAME: {referCustomer_name}</p>
+            <p>MOBILE: {referCustomer_phone}</p>
+            <p>REFERAL CODE: {referCustomer_code}</p>
+            <p>RECEIPT: {referCustomer_receipt}</p>
+            <p>DEAL DATE: {new Date(created_at).toLocaleDateString()}</p>
+            <p>
+                FIRST 15 DAYS DEALS: {ReferCustomer_deals_totalpricefirst15days}
+            </p>
+            <p>
+                LAST 15 DAYS DEALS: {ReferCustomer_deals_totalpricelast15days}
+            </p>
+            <p>
+                CASHBACK:{' '}
+                {(
+                    Math.round(
+                        (ReferCustomer_deals_totalpricefirst15days * 0.1 +
+                            ReferCustomer_deals_totalpricelast15days * 0.05) *
+                            100
+                    ) / 100
+                ).toFixed(2)}{' '}
+                L.E
+            </p>
+        </div>
+    );
 }
